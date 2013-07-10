96a0749dc611063593cb35b1ad346ffc83901a2e
Jeramie <jbrehm@fullsail.edu>
2013-07-10 01:27:13 -0400

    initial fight function run

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -10,5 +10,30 @@ var pOneHp = 100;
 var pTwoHp = 100;
 
 //player max damage per round
-var pOneDmg = 25;
-var pTwoDmg = 25;
+var pOneDmg = Math.floor((Math.random()*100)+1);
+var pTwoDmg = Math.floor((Math.random()*100)+1);
+
+//Round
+var round = 0;
+
+//functions
+
+
+//fight
+//function variables p1Hp= player 1 health points, p1Max = player 1 max damage
+var fightFn = function(p1Hp, p1Max, p2Hp, p2Max){
+	var newp1Hp = (p1Hp - p2Max);
+	var newp2Hp = (p2Hp - p1Max);
+	round++;
+	return newp1Hp, newp2Hp, round;
+	
+};
+
+//winner check
+
+var winnerFn = function(){
+	
+};
+
+fightFn(pOneHp, pOneDmg, pTwoHp, pTwoDmg);
+console.log(newp1hp, newp2hp, round);
