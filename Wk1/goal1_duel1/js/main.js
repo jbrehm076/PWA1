ccf5a21cdac86218192f562417f04d22a2a30f5c
Jeramie <jbrehm@fullsail.edu>
2013-07-10 00:32:02 -0400

    est. player variables

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -1,1 +1,14 @@
 // Assignment: The Duel - Part I, Jeramie Brehm 10JUL13
+
+//game variables
+//players
+var pOneName = "Beavis";
+var pTwoName = "Butthead";
+
+//player health variables
+var pOneHp = 100;
+var pTwoHp = 100;
+
+//player max damage per round
+var pOneDmg = 25;
+var pTwoDmg = 25;
