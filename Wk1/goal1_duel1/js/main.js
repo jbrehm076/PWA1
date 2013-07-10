bcefbabbd3ac2d57bf03f9d09f29e94157168c7a
Jeramie <jbrehm@fullsail.edu>
2013-07-10 04:52:03 -0400

    added comments

--- Wk1/goal1_duel1/js/main.js
+++ Wk1/goal1_duel1/js/main.js
@@ -9,7 +9,7 @@ var pTwoName = "Butthead";
 var pOneHp = 100;
 var pTwoHp = 100;
 
-//player max damage per round
+//player max damage per round and random number generators
 var pOneDmg = Math.floor((Math.random()*25)+1);
 var pTwoDmg = Math.floor((Math.random()*25)+1);
 
@@ -36,14 +36,14 @@ function winnerFn(name1, hp1, name2, hp2){
 	var hp2;
 	var name1;
 	var name2;
-	if((hp1 <= 0) || (hp2 <= 0)){
+	if((hp1 <= 0) || (hp2 <= 0)){	//if both are at or below 0
 		if((hp1 <= 0) && (hp2 <= 0)){
 			alert("No Winner");
-		}else if(hp1 <= 0){
+		}else if(hp1 <= 0){  	//compare player 1 hp
 			alert("GAME OVER " + name2 + " Wins!!!");
 		}else
-			alert("GAME OVER " + name1 + " Wins!!!");
-	}else if(round == 10){
+			alert("GAME OVER " + name1 + " Wins!!!"); //player det. to be winner
+	}else if(round == 10){	//if both players have above 0 hp and round ten is reached it is a draw.
 		alert("No Winner");
 	}else{
 		alert(name1 + ": " + hp1 + " **ROUND " + round + " OVER** " + name2 + ": " + hp2);
