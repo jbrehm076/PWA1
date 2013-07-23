cf80a006716ac4a6c8ca88d65ab65a9940499c94
Jeramie <jbrehm@fullsail.edu>
2013-07-22 23:36:52 -0400

    switch loop implemented. fight number generator works.

--- Wk2/goal4_assign_duel3/js/main.js
+++ Wk2/goal4_assign_duel3/js/main.js
@@ -57,26 +57,50 @@ var p2Health = fighter2.health;
 document.querySelector('#kabal p').innerHTML = fighter1.name + " : " + 100;
 document.querySelector('#kratos p').innerHTML = fighter2.name + " : " + 100;
 
-for(var i=0, maxRounds=10; i<maxRounds;i++){
+	var round = 1
 	fight_btn.onclick = function(e){
+		
 		var beavis = document.querySelector('#kabal');
 		var butthead = document.querySelector('#kratos p');
 		p1dam = dmgGen(25);
 		p2dam = dmgGen(25);
 		
+		console.log(p1dam);
+		console.log(p2dam);
+		
 		p1Health = p1Health - p2dam;
 		p2Health = p2Health - p1dam;
+		
+		document.querySelector('#kabal p').innerHTML = fighter1.name + " : " + p1Health;
+		document.querySelector('#kratos p').innerHTML = fighter2.name + " : " + p2Health;
+		document.querySelector('#round').innerHTML = "ROUND " + round + " COMPLETE";
+		
+		switch(rCheck(p1Health,p2Health,round)){
+			
+			case 1:
+			//beavis wins
+			
+			case 2:
+			//butthead wins
+			
+			case 3:
+			//both dead
+			
+			//both alive but fight over.
+			case 4:
+			
+		
 		
 	
 	
 	
 	
-	
-	
+		};
+	round++;
 	};
 	//e.preventDefault();
 	//return false;
-};
+
 /*
 // setup variables for later use
 var fButton	= document.querySelector('#fight_btn');		//creates variable for button id			
