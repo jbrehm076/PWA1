e417ba4eaaa67432a2a432c52b6c89f4fc210c2b
Jeramie <jbrehm@fullsail.edu>
2013-07-23 21:38:19 -0400

    added several functions. periodic update

--- Wk2/goal4_assign_guess/js/main.js
+++ Wk2/goal4_assign_guess/js/main.js
@@ -11,4 +11,26 @@
 (function (){
 
 
+var guessButton = document.querySelector('body button');
+
+
+////////////functions//////////////////
+function inputVal(num){
+
+
+};
+
+function randomNum(){
+	var randomNum = Math.floor((Math.random()*10)+1);
+	return randomNum;
+};
+
+function evaluate(){
+console.log('booty');
+};
+console.log(document.querySelector('body button'));
+
+
+guessButton.addEventListener('click', evaluate, false);
+
 })();
