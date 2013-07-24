2137604e33d2b84d0fec4f8ada98c229d6341822
Jeramie <jbrehm@fullsail.edu>
2013-07-23 23:13:39 -0400

    added variables and portion of number comparison code.

--- Wk2/goal4_assign_guess/js/main.js
+++ Wk2/goal4_assign_guess/js/main.js
@@ -13,17 +13,54 @@
 
 var button = document.querySelector('body button');
 var randomNum = Math.floor((Math.random()*10)+1);
-console.log(randomNum);
+var output = document.getElementById('output').innerHTML;
+var guessRem = 3;
+
+function enterKey(){
+	var whichKey;
+		if (key === 13){
+			validate();
+		};
+};
+
+
+function validate(input){
+	var validity;
+	
+	if (isNaN('input') = true){
+		
+		validity = 0;
+		output = "Ah, ah, ah, you have to enter a NUMBER to be able to guess my magic number. Please try again.";
+		return validity;
+		
+	}else if((input < 0) || (input > 10)){
+		
+		validity = 1;
+		output = "C'mon, work with me now... the number has to be BETWEEN 1 and 10 for this to work. Please try again.";
+		return validity;
+		
+	}else{
+		
+		validity = 2;
+		return validity;
+		
+	};
+};
+
 
 function evaluate(guess){
-guessInput = parseInt(document.querySelector('#input').value);	
-
-console.log(guessInput);
+guessInput = parseInt(document.querySelector('#input').value);
+var guessV = validate(guessInput);
+	if (guessV === 2){
+		if(guessInput === randomNum){
+		
+	
 
 };
 
 //text output // document.getElementById('output').innerHTML
 button.addEventListener('click', evaluate, false);
+//button.addEventListener('keypress', enterKey(), false);
 
 
 
