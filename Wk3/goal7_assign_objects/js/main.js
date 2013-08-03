89f8cd6544003359c43fb09c87c66cac79aed9ae
Jeramie <jbrehm@fullsail.edu>
2013-08-03 10:38:13 -0400

    initial code

--- Wk3/goal7_assign_objects/js/main.js
+++ Wk3/goal7_assign_objects/js/main.js
@@ -4,3 +4,24 @@
  * Goal: Goal7
  */
 
+var names = ['Slim','Shady','Brody','Cody','Goober'];
+var people = [];
+
+
+for (i=0,a=names.length;i<a;i++){
+	var ranNum = Math.floor((Math.random()*names.length)+1);
+	people.splice(1,0,names[ranNum]);
+		
+		console.log(people);
+		console.log(names);
+};
+	
+function Person(namee,actionn,jobb,roww){
+	this.name = namee;
+	this.action = actionn;
+	this.job = jobb;
+	this.row = row;
+};
+	
+	
+
